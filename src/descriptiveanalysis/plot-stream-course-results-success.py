b0c07ade11075fa6248bbfad085071cbda1dc4df
DushyanthaWidanagamage <Dushyantha.20@cse.mrt.ac.lk>
2020-03-15 01:23:17 +0800

    Descriptive analysis - Correct comments in plot-stream course success file

--- src/descriptiveanalysis/plot-stream-course-results-success.py
+++ src/descriptiveanalysis/plot-stream-course-results-success.py
@@ -10,8 +10,9 @@ import seaborn as sns
 
 #This uses csv file in data folder called student-analyase-succes-stream-course-results.csv
 #Used this code to identify best variable that can help explain student future success
-#Here result scale was identified as a nominal variable and course-catagory and and stream as nominal data.
-#Hence I have observed that, the student that score well in their subjects has a good chance in the future to become successful.
+#Here students A/L result was identified as a ordinal variable and course-catagory and and stream as nominal data.
+#Hence I have observed that, the student that score well in their A/L have a good chance in becoming successful in the future. 
+# I have ignored gender and age because they are demographic data.
 
 # Input : student-analyase-succes-stream-course-results.csv in data folder 
 
