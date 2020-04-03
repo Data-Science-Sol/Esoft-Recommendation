496ae3cb15c6cbad64030cfac9a71531f01836b6
DushyanthaWidanagamage <Dushyantha.20@cse.mrt.ac.lk>
2020-04-04 00:43:14 +0800

    Add gender for analysis

--- src/datatypesrelationships/PairwiseAttributeAnalysis.py
+++ src/datatypesrelationships/PairwiseAttributeAnalysis.py
@@ -6,15 +6,19 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(style="darkgrid")
 
-input : preprocessed_student_details_f1.csv
-output : results\datatypesrelationships\pairwiserelationships.jpg
+#Input preprocessed_student_details_f1.csv
+#Results : results\datatypesrelationships\pairwiserelationships.JPG
 
-df = pd.read_csv("preprocessed_student_details_f1.csv")
+df = pd.read_csv("/content/preprocessed_student_details_f1.csv")
 
-cleanup_nums = {"Course":     {"Software Engineering": 1, "Application Development": 2 , "Network Engineering" : 3 , "Accounting" : 4, 
+cleanup_nums = {"Course":  {"Software Engineering": 1, "Application Development": 2 , "Network Engineering" : 3 , "Accounting" : 4, 
                                "Business Management" : 5 }}
 
 df.replace(cleanup_nums, inplace=True)
+
+cleanup_nums1 = {"Gender":  {"Male": 1, "Female": 2 }}
+
+df.replace(cleanup_nums1, inplace=True)
 
 g = sns.pairplot(df, kind="reg")
 
